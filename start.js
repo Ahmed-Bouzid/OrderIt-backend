f8fe6a1fc2d79afcad353ba29b446785d23051f0
BOUZID Ahmed <ahmahm@hotmail.co.jp>
2026-02-09 00:48:29 +0100

    🔧 FIX: URLs backend - sunnygo → orderit

--- start.js
+++ start.js
@@ -24,7 +24,7 @@ const io = new Server(server, {
 					// Production : CORS STRICT (liste blanche)
 					origin: [
 						"https://sunnygo-frontend.vercel.app", // Frontend production
-						"https://sunnygo-backend-6y1m.onrender.com", // Backend production
+					"https://orderit-backend-6y1m.onrender.com", // Backend production
 						process.env.FRONTEND_URL, // Variable d'environnement
 					].filter(Boolean), // Enlever les undefined
 					credentials: true,
